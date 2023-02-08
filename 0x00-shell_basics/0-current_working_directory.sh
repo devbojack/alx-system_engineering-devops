390d8c3e1bd18167ac95d26a175f1bbce2541390
Justus Lolwerikoi <dev.bojack@gmail.com>
2023-02-08 07:20:30 -0800

    Added a personal script for pwd

--- 0x00-shell_basics/0-current_working_directory.sh
+++ 0x00-shell_basics/0-current_working_directory.sh
@@ -1,4 +1,2 @@
 #!/bin/bash
-path=$(realpath "${BASH_SOURCE:-$0}")
-DIR_PATH=$(dirname $path)
-echo $DIR_PATH
+echo $(dirname $(realpath "${BASH_SOURCE:-$0}"))
---
+++ README.md
@@ -0,0 +1,1 @@
+My first system engineering devops project
