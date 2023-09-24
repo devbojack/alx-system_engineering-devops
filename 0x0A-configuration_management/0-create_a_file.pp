e290b4af53d2e831651c70db02016b475ca94e29
Justus Lolwerikoi <dev.bojack@gmail.com>
2023-09-24 07:11:16 -0700

    create config file

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,8 +1,9 @@
 # Create a file in /tmp
+
 file { '/tmp/school':
   ensure  => 'present',
   content => 'I love Puppet',
-  mode    => '0744',
-  owner   => 'www.data',
-  group   =>  'www.data',
+  owner   => 'www-data',
+  group   => 'www-data',
+  mode    => '0744'
 }
