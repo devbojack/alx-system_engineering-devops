ebabf7a95e577b854054ad3073eba593a2704ee2
Justus Lolwerikoi <dev.bojack@gmail.com>
2023-09-24 08:06:11 -0700

    Install a package

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,14 +1,6 @@
 #Install flask from pip3 using puppet
 
-exec { 'install_flask':
-  command => '/usr/bin/pip3 install flask==2.1.0',
-}
-
-package { 'python3_pip':
-  ensure => 'installed',
-}
-
-package { 'flask':
-  ensure => 'installed',
-  before => Exec['install-flask']
+package {'flask':
+  ensure   => '2.1.0',
+  provider => 'pip3'
 }
