6ccb4b9f0ae6e74498945ae3a892f60c666dff87
dev.bojack <98508704+devbojack@users.noreply.github.com>
2023-11-07 20:24:49 +0300

    How many subs?

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+
+"""
+queries the Reddit API and returns the number of 
+subscribers for a given subreddit.
+"""
+import requests
+
+def number_of_subscribers(subreddit):
+    url = f"https://www.reddit.com/r/{subreddit}/about.json"
+    headers = {'User-Agent': 'My Reddit API Client'}
+
+    try:
+        response = requests.get(url, headers=headers)
+
+        if response.status_code == 200:
+            data = response.json()
+            subscribers = data['data']['subscribers']
+            return subscribers
+        else:
+            return 0
+    except Exception as e:
+        return 0
+
+if __name__ == '__main__':
+    import sys
+    if len(sys.argv) < 2:
+        print("Please pass an argument for the subreddit to search.")
+    else:
+        subreddit = sys.argv[1]
+        num_subscribers = number_of_subscribers(subreddit)
+        print(f"{num_subscribers}")
