a48f00937545363898bcbf290a42694bc52a41a1
dev.bojack <98508704+devbojack@users.noreply.github.com>
2023-11-08 21:51:09 +0300

    strace

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,16 +1,6 @@
+# Pupper fix
 
-service { 'apache2':
-  ensure => 'running',
-}
-
-file { '/etc/apache2/sites-available/your-site.conf':
-  ensure  => present,
-  content => template('your-site-config.erb'),
-  notify  => Service['apache2'],
-}
-
-file { '/etc/apache2/sites-available/your-site-config.erb':
-  ensure  => present,
-  content => template('your-site-config.erb'),
-  notify  => Service['apache2'],
+exec { 'fix-wordpress':
+  command => "/bin/sed -i /var/www/html/wp-settings.php \
+  -e 's/class-wp-locale.phpp/class-wp-locale.php/'"
 }
