761b240686ad0683047353f563170b230366f4b0
dev.bojack <98508704+devbojack@users.noreply.github.com>
2023-11-26 14:06:35 +0300

    repetition token fix

--- 0x06-regular_expressions/3-repetition_token_2.rb
+++ 0x06-regular_expressions/3-repetition_token_2.rb
@@ -1,3 +1,3 @@
 #!/usr/bin/env ruby
-# Prints all the words that start with h, end with n, and have any single character in between.
+# Prints all the words that start with 'h', end with 'n', and have any single character in between
 puts ARGV[0].scan(/hbt+n/).join
